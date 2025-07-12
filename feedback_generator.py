105c89e8cfb339ec643fe643d4bc4d2463905f81
Reece Luo <reeceluo@Reeces-MacBook-Air-2.local>
2025-07-12 02:20:38 -0500

    Updated exemplar extracting function

--- feedback_generator.py
+++ feedback_generator.py
@@ -17,13 +17,64 @@ model = genai.GenerativeModel("models/gemini-2.5-flash")        # instantiate mo
 
 
 
-def generate_feedback():
+# Setup pose extraction
+pose = mp.solutions.pose.Pose()     # Initialize MediaPipe Pose
 
-    prompt = f"""
 
-"""
+def determine_key_landmarks(): 
 
-def get_pose_landmarks():
     keypoints = {
-
+        "nose": mp.solutions.pose.PoseLandmark.NOSE,
+        "left_eye": mp.solutions.pose.PoseLandmark.LEFT_EYE,
+        "right_eye": mp.solutions.pose.PoseLandmark.RIGHT_EYE,
+        "left_ear": mp.solutions.pose.PoseLandmark.LEFT_EAR,
+        "right-ear": mp.solutions.pose.PoseLandmark.RIGHT_EAR,
+        "left_shoulder": mp.solutions.pose.PoseLandmark.LEFT_SHOULDER,
+        "right_shoulder": mp.solutions.pose.PoseLandmark.RIGHT_SHOULDER,
+        "left_elbow": mp.solutions.pose.PoseLandmark.LEFT_ELBOW,
+        "right_elbow": mp.solutions.pose.PoseLandmark.RIGHT_ELBOW,
+        "left_wrist": mp.solutions.pose.PoseLandmark.LEFT_WRIST,
+        "right_wrist": mp.solutions.pose.PoseLandmark.RIGHT_WRIST,
+        "left_hip": mp.solutions.pose.PoseLandmark.LEFT_HIP,
+        "right_hip": mp.solutions.pose.PoseLandmark.RIGHT_HIP,
+        "left_knee": mp.solutions.pose.PoseLandmark.LEFT_KNEE,
+        "right_knee": mp.solutions.pose.PoseLandmark.RIGHT_KNEE,
+        "left_ankle": mp.solutions.pose.PoseLandmark.LEFT_ANKLE,
+        "right_ankle": mp.solutions.pose.PoseLandmark.RIGHT_ANKLE,
+        "left_heel": mp.solutions.pose.PoseLandmark.LEFT_HEEL,
+        "right_heel": mp.solutions.pose.PoseLandmark.RIGHT_HEEL,
+        "left_foot_index": mp.solutions.pose.PoseLandmark.LEFT_FOOT_INDEX,
+        "right_foot_index": mp.solutions.pose.PoseLandmark.RIGHT_FOOT_INDEX
     }
+
+
+    return
+
+
+def extract_exemplar_landmarks():
+    cap = cv2.VideoCapture("dougie.mp4")
+    if not cap.isOpened():
+        print("Error: video file not opened.")
+        exit()
+    
+    exemplar_landmarks = []
+
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+        
+        # convert frames from BGR to RGB (for MediaPipe)
+        frame_in_rbg = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+        frame_landmarks = pose.process(frame_in_rbg)
+
+        if frame_landmarks.pose_landmarks:
+            print("Stopped here") # DUMMY CODE
+
+ 
+    return exemplar_landmarks
+
+
+def generate_feedback():
+    return
