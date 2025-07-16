9575d9a9fd8eea24f5f3362f416dd84faf6d9fa0
Reece Luo <reeceluo101@gmail.com>
2025-07-16 01:56:06 -0500

    Rename TeachMeHowToDougie.py to app.py

--- app.py
+++ app.py
@@ -16,8 +16,6 @@ GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 genai.configure(api_key = GEMINI_API_KEY)
 model = genai.GenerativeModel("models/gemini-2.5-flash")        # instantiate model
 
-
-
 # Setup pose extraction
 pose = mp.solutions.pose.Pose()     # Initialize MediaPipe Pose
 
@@ -63,8 +61,7 @@ def get_keypoints(landmark):
 def extract_exemplar_keypoints():
     cap = cv2.VideoCapture("dougie.mp4")
     if not cap.isOpened():
-        print("Error: video file not opened.")
-        exit()
+        raise RuntimeError("Error: video file not opened.")
     
     exemplar_keypoint_sequence = []
 
@@ -75,11 +72,10 @@ def extract_exemplar_keypoints():
         
         # convert frames from BGR to RGB (for MediaPipe)
         frame_in_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-
         processed_frame = pose.process(frame_in_rgb)    # processes RGB image and returns a NamedTuple pose landmarks
-
+        
         mp.solutions.drawing_utils.draw_landmarks(frame, processed_frame.pose_landmarks, mp.solutions.pose.POSE_CONNECTIONS)   # draws landmarks and connections
-
+        
         cv2.imshow("The Dougie", frame)
 
         if processed_frame.pose_landmarks is not None:
@@ -99,10 +95,31 @@ def get_dance_keypoints():
 
 
 def generate_feedback(user_keypoint_sequence, exemplar_keypoint_sequence):
-    return
+    
+    
+    prompt = f"""
+    You are a dance coach teaching the user how to do the popular hip-hop dance called "The Dougie". You are given
+    two sequences of joint positions: the first extracted from an exemplar of "The Dougie" dance, and the second from
+    the user.
+
+        - Each line contains normalized 
+        - 
+        - 
+    
+    Exemplar sequence:
+    {exemplar_keypoint_sequence}
+
+    User sequence:
+    {user_keypoint_sequence}
+    """
+
+    response = model.generate_content(prompt)
+    return response.text
 
 
 # Displays countdown on webcam for user
+# params: bool for if countdown is active, time countdown started, duration of countdown, frame
+# return: bool for if countdown is still active
 def countdown(countdown_active, countdown_start_time, countdown_seconds, frame):
     elapsed_time = time.time() - countdown_start_time
     remaining = countdown_seconds - int(elapsed_time)
@@ -142,11 +159,15 @@ countdown_active = False
 countdown_start_time = None
 countdown_seconds = 3
 
-# 
+# User's dougie fields
 hit_da_dougie = False
 dougie_start_time = None
 dougie_duration = 3
 user_keypoint_sequence = []
+
+# Feedback fields
+generating_feedback = False
+
 
 while cap.isOpened():
 
@@ -176,6 +197,7 @@ while cap.isOpened():
 
         if elapsed_time > dougie_duration:
             hit_da_dougie = False
+            generating_feedback = True
 
         elif elapsed_time > dougie_duration - 1:
             cv2.putText(frame,
@@ -189,14 +211,18 @@ while cap.isOpened():
 
     cv2.imshow("Webcam", frame)
 
-    key = cv2.waitKey(1) & 0xFF     # Only called once
+    if generating_feedback:
+        generate_feedback(user_keypoint_sequence, exemplar_keypoint_sequence)
+    else:
+        key = cv2.waitKey(1) & 0xFF     # Only called once
 
-    if key == ord('q'):   # 0xFF is a hexadecimal, statement checks if ASCII value of key pressed is 'q's
-        break
+        if key == ord('q'):   # 0xFF is a hexadecimal, statement checks if ASCII value of key pressed is 'q's
+            break
 
-    if key == ord('r'):   # run countdown if 'r' is pressed
-        countdown_active = True
-        countdown_start_time = time.time()
+        if key == ord('r'):   # run countdown if 'r' is pressed
+            countdown_active = True
+            countdown_start_time = time.time()
+            user_keypoint_sequence = []         # reset user keypoint sequence
 
 
 
